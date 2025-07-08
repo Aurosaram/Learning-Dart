a6c8869e5265ae2aa63ecb95ca4434332dd7d2f2
Aurosaram <68229014+Aurosaram@users.noreply.github.com>
2025-07-08 23:04:15 +0400

    adding file number 5

---
+++ 5.five.dart
@@ -0,0 +1,24 @@
+// collections
+
+void main() {
+  // list
+  List names = ['Alice', 'Bob', 'Charlie', 100, true, 4.6];
+  print(names[0]); // Alice
+
+  names[0] = 'David';
+
+  for (var name in names) {
+    print(name);
+  }
+
+  // copy list
+  List names2 = [...names];
+  names[0] = 'Eve'; // names2 is not affected
+  print(names2[0]); // David
+
+  // specific type list and const
+  List<String> names3 = const ['Alice', 'Bob', 'Charlie'];
+  // the list type is String, so it can only contain strings
+  // names3[0] = 'David'; // This will throw an error because names3 is a const list
+  print(names3[0]); // Alice
+}
