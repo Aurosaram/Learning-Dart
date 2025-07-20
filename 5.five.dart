5123fc6cafa19ccb97deb6d152ef44738fe4be27
Aurosaram <68229014+Aurosaram@users.noreply.github.com>
2025-07-20 16:44:13 +0400

    collections

--- 5.five.dart
+++ 5.five.dart
@@ -21,4 +21,23 @@ void main() {
   // the list type is String, so it can only contain strings
   // names3[0] = 'David'; // This will throw an error because names3 is a const list
   print(names3[0]); // Alice
+
+  // set
+  var uniqueNames = {'Alice', 'Bob', 'Charlie', 'Alice'};
+  print(uniqueNames); // {Alice, Bob, Charlie}
+
+  // emptu set
+  var emptySet = <String>{};
+  print(emptySet.isEmpty); // true
+
+  // map
+  var person = {'name': 'Alice', 'age': 30, 'isStudent': false};
+  print(person['name']); // Alice
+  person['age'] = 31; // update value
+
+  // empty map
+  var emptyMap = Map();
+  print(emptyMap.isEmpty); // true
+  emptyMap['key'] = 'value';
+  print(emptyMap); // {key: value}
 }
